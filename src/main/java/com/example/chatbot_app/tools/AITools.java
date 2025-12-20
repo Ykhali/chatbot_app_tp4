fc2c06d37289134521979e8df8f74a8c1fe38899
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-12-20 15:47:40 +0100

    Add Mcp tools

--- src/main/java/com/example/chatbot_app/tools/AITools.java
+++ src/main/java/com/example/chatbot_app/tools/AITools.java
@@ -8,19 +8,7 @@ import java.util.List;
 
 @Component
 public class AITools {
-    @Tool(name = "getEmployee", description = "Get information about a given employee")
-    public Employee getEmployee(@ToolParam(description = "The employee name") String name) {
-        return new Employee(name, 12300, 4);
-    }
 
-    @Tool(description = "Get All Employees")
-    public List<Employee> getAllEmployees() {
-        return List.of(
-                new Employee("Hassan", 12300, 4),
-                new Employee("Mohamed", 34000, 1),
-                new Employee("Imane", 23000, 10)
-        );
-    }
 }
 
 record  Employee(String name, double salary, int seniority){
