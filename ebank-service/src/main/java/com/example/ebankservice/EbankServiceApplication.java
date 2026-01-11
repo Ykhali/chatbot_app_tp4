4897e819060c6e9f1977f0e082f9ff307098538d
Ykhali <Youssef.Khalid@emsi-edu.ma>
2026-01-12 00:36:44 +0100

    add bean in EbankServiceApplication.java

--- ebank-service/src/main/java/com/example/ebankservice/EbankServiceApplication.java
+++ ebank-service/src/main/java/com/example/ebankservice/EbankServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.ebankservice;
 
+import com.example.ebankservice.entities.BankAccount;
+import com.example.ebankservice.services.BankAccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EbankServiceApplication {
@@ -10,4 +14,16 @@ public class EbankServiceApplication {
         SpringApplication.run(EbankServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(BankAccountService bankAccountService) {
+        return args -> {
+            for (int i=0; i<10; i++){
+                BankAccount bankAccount = BankAccount.builder()
+                        .balance(1000+Math.random()*6098765)
+                        .type(Math.random()>0.5?"CURRENT-ACCOUNT":"SAVING-ACCOUNT")
+                        .build();
+                bankAccountService.saveAccount(bankAccount);
+            }
+        };
+    }
 }
