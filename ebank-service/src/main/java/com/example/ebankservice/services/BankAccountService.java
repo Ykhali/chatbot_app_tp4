4d9327ef5404b1b4df8737b134f395f8a45b4712
Ykhali <Youssef.Khalid@emsi-edu.ma>
2026-01-12 00:54:08 +0100

    18th commit

--- ebank-service/src/main/java/com/example/ebankservice/services/BankAccountService.java
+++ ebank-service/src/main/java/com/example/ebankservice/services/BankAccountService.java
@@ -2,6 +2,8 @@ package com.example.ebankservice.services;
 
 import com.example.ebankservice.entities.BankAccount;
 import com.example.ebankservice.repo.BankAccountRepository;
+import org.springaicommunity.mcp.annotation.McpArg;
+import org.springaicommunity.mcp.annotation.McpTool;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,16 +18,19 @@ public class BankAccountService {
         this.bankAccountRepository = bankAccountRepository;
     }
 
+    @McpTool(description = "Get All Bank Accounts")
     public List<BankAccount> getAllAccounts() {
         return bankAccountRepository.findAll();
     }
 
-    public BankAccount getBankAccountById(String id) {
+    @McpTool(description = "Get A Bank Account By Id")
+    public BankAccount getBankAccountById(@McpArg(description = "The bank account id") String id) {
         return bankAccountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Bank account not found"));
     }
 
-    public BankAccount saveAccount(BankAccount bankAccount) {
+    @McpTool(description = "Add new Bank Account")
+    public BankAccount saveAccount(@McpArg(description = "New Bank Account (id, createdAt, balance, type)") BankAccount bankAccount) {
         bankAccount.setCreatedAt(new Date());
         bankAccount.setId(UUID.randomUUID().toString());
         return bankAccountRepository.save(bankAccount);
